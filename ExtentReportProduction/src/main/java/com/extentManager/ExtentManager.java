662a78f027d91106a270f938481e7d129b086540
dell <dell@Abhi2>
2024-11-08 16:31:54 +0530

    Change in Reports Name under ExtentManager.java line number - 18.

--- ExtentReportProduction/src/main/java/com/extentManager/ExtentManager.java
+++ ExtentReportProduction/src/main/java/com/extentManager/ExtentManager.java
@@ -16,7 +16,7 @@ public class ExtentManager {
 	public static void setExtent() throws IOException {
 
 		sparkReporter = new ExtentSparkReporter(System.getProperty("user.dir") + "\\test-output\\ExtentReport\\"
-				+ "Report_" + BaseClass.getCurrentTime() + ".html");
+				+ "Test_Report.html");
 		sparkReporter.loadXMLConfig(System.getProperty("user.dir") + "/extent-config.xml");
 
 		extent = new ExtentReports();
